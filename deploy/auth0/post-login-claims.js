e5d6ffe0a23a4bd98b79de9e9acf0dfc616a491b
Deniz Mutlu <denizmine38@hotmail.com>
2026-09-15 18:01:06 +0300

    chore: align auth0 action with the deployed version

--- deploy/auth0/post-login-claims.js
+++ deploy/auth0/post-login-claims.js
@@ -1,16 +1,45 @@
-const CLAIM_NAMESPACE = "https://ecommerce.local/claims";
-
+/**
+ * Adds the application profile and authorization claims to Auth0 tokens.
+ *
+ * @param {Event} event
+ * @param {PostLoginAPI} api
+ */
 exports.onExecutePostLogin = async (event, api) => {
-  const email = event.user.email;
-  const emailVerified = event.user.email_verified === true;
-  const displayName = event.user.name || event.user.nickname || email;
-  const roles = event.authorization?.roles || [];
+  const namespace = "https://ecommerce.local/claims/";
 
-  if (event.authorization) {
-    api.accessToken.setCustomClaim(`${CLAIM_NAMESPACE}/roles`, roles);
+  if (event.user.email) {
+    api.accessToken.setCustomClaim(
+      `${namespace}email`,
+      event.user.email
+    );
   }
 
-  api.accessToken.setCustomClaim(`${CLAIM_NAMESPACE}/email`, email);
-  api.accessToken.setCustomClaim(`${CLAIM_NAMESPACE}/email_verified`, emailVerified);
-  api.accessToken.setCustomClaim(`${CLAIM_NAMESPACE}/name`, displayName);
+  api.accessToken.setCustomClaim(
+    `${namespace}email_verified`,
+    event.user.email_verified === true
+  );
+
+  const name =
+    event.user.name ||
+    event.user.nickname ||
+    event.user.email;
+
+  if (name) {
+    api.accessToken.setCustomClaim(
+      `${namespace}name`,
+      name
+    );
+  }
+
+  const roles = event.authorization?.roles ?? [];
+
+  api.accessToken.setCustomClaim(
+    `${namespace}roles`,
+    roles
+  );
+
+  api.idToken.setCustomClaim(
+    `${namespace}roles`,
+    roles
+  );
 };
